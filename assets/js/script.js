12d92ac3309b23de98351d7d58d13ece5072d718
zambra87 <daniel.zambra@me.com>
2024-07-13 00:20:03 -0400

    Se agrega footer, script para dezplazamiento smooth y ajustes menores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl)) /* Se activan los popover*/
+
+
+<script>
+$(document).ready(function() { /* Script para desplazamiento suave desde el menu */
+
+    $('.navbar-nav a').on('click', function(e) {
+        e.preventDefault();
+        
+        var target = $(this).attr('href');
+        
+        /* Desplazamiento suave animado hasta el elemento objetivo */
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - 70 
+        }, 1000); /* 1 segundo de duracion */
+    });
+});
+</script>
